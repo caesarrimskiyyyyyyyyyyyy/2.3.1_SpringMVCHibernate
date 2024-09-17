b183d250f0bf7d3692e2d1114e314020b7321f90
caesarrimskiyyyyyyyyyyyy <caesarrimskiy@gmail.com>
2024-09-17 17:09:14 +0300

    add lombok

--- src/main/java/com/example/web/model/User.java
+++ src/main/java/com/example/web/model/User.java
@@ -1,5 +1,7 @@
 package com.example.web.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Column;
 
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -15,77 +18,12 @@ public class User {
     @Column(name = "id")
     private long id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
 
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
     @Column(name = "email", nullable = false)
     private String email;
-
-    @Column(name = "company")
-    private String company;
-
-    @Column(name = "job_title")
-    private String jobTitle;
-
-    public User() {
-    }
-
-    public User(String name, String lastName, String email, String company, String jobTitle) {
-        this.name = name;
-        this.lastName = lastName;
-        this.email = email;
-        this.company = company;
-        this.jobTitle = jobTitle;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
 }
