edead6719a20ed49e23d700f35c7f99ede6bb9fc
caesarrimskiyyyyyyyyyyyy <caesarrimskiy@gmail.com>
2024-09-26 15:00:48 +0300

    Fix annotation @Data

--- src/main/java/com/example/web/model/User.java
+++ src/main/java/com/example/web/model/User.java
@@ -1,6 +1,8 @@
 package com.example.web.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,9 +11,11 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Column;
 
-@Data
 @Entity
 @Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
